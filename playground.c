ea74198edb93499e59f34303869874b00e3d3fd3
Kurt Bermudez <kurt@wizy.io>
2023-12-19 00:12:49 +0800

    + playground

---
+++ playground.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!!!\n");
+    printf("Nilagyan ko lang to para may ma-commit ako sa github dahil yung "
+           "tutorial na sinusundan ko ay nag-eexplain ng puro under the hood\n");
+    printf("Which is FUN\n");
+    return 123;
+}
